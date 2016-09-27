41e45fce2015a65f092be9211d59458e0dfaf3f2
SprinLa <631857976@qq.com>
2016-09-27 11:37:07 +0800

    1.更改hdfs路径

--- src/main/java/SparkDemoHDFS.java
+++ src/main/java/SparkDemoHDFS.java
@@ -13,7 +13,7 @@ public class SparkDemoHDFS {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("SparkDemo");//SparkConf对象包含了Spark应用的一些列信息
         JavaSparkContext sc = new JavaSparkContext(conf);//初始化spark,创建SparkContext对象,指定spark访问集群的方式.
-        JavaRDD<String> rddFile = sc.textFile("/user/hdfs/rawlog/www_sinaedgeahsolci14ydn_trafficserver/2016_09_23/00/www_sinaedgeahsolci14ydn_trafficserver-yf235028.scribe.dip.sina.com.cn_16469-2016_09_23_00-20160923011_00000 ");//该数据集没有加载到内存，rddFile仅仅是一个指向文件的指针。
+        JavaRDD<String> rddFile = sc.textFile("/user/hdfs/rawlog/www_sinaedgeahsolci14ydn_trafficserver/2016_09_23/00/");//该数据集没有加载到内存，rddFile仅仅是一个指向文件的指针。
 
         JavaRDD<Integer> lineLengths =rddFile.map(s->s.length());//此时lineLengths也没有进行运算，因为map操作为懒执行。
         lineLengths.cache();//使用默认存储级别的快捷设置方法.默认StorageLevel.MEMORY_ONLY,将反序列化的对象存储到内存中.
